12814f8df95adc953ba9b18eb5b1db14dd58309e
Hack Reactor Student <student@hackreactor.com>
2017-06-02 20:07:32 -0700

    completed listing of videos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 // TODO: Render the `App` component to the DOM
 
-ReactDOM.render(<window.App />, document.getElementById("app"));
+ReactDOM.render(<window.App />, document.getElementById( 'app' ));
